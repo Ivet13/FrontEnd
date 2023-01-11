15f84de6ba06b8c1f4f7a77c700bfd221fed08c4
TiborKopca <tibor.kopca@gmail.com>
2023-01-11 16:53:49 +0100

    footer test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router";      //index.js
 import AppLink from "./components/AppLink.vue";
 
 import "./../node_modules/bulma/css/bulma.css";
